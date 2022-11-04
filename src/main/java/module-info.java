81da25e3aa942b19f05c3716654e409611a40ef7
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2022-11-04 12:40:14 -0700

    Remove unneeded javafx.fxml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,5 @@ module sierpinski {
     requires javafx.controls;
     requires transitive javafx.graphics;
 
-    opens com.megabyte6.sierpinski to javafx.fxml;
-
     exports com.megabyte6.sierpinski;
 }
